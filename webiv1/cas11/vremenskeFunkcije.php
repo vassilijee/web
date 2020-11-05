814649e550f9f21d122fb4bb0bb160b957772806
vassilijee <45370993+vassilijee@users.noreply.github.com>
2020-11-05 10:32:44 +0100

    Add files via upload

--- webiv1/cas11/vremenskeFunkcije.php
+++ webiv1/cas11/vremenskeFunkcije.php
@@ -4,9 +4,19 @@
 
 // date()
 
-$datum = date("d/m/Y H:i:s");
+$datum = date("d/m/Y H:i:s", 1604568342); // drugi parametar je neko vreme u sekundama od 1.1.1970
 var_dump($datum);
 
 $vremeS = time();
 echo "<br>";
 var_dump($vremeS);
+
+//mktime()
+
+$vremeMK = mktime(8, 02, 22, 13, 10, 1994);
+echo "<br>";
+var_dump($vremeMK);
+
+$datum2 = date("d/m/Y H:i:s", $vremeMK); // drugi parametar je neko vreme u sekundama od 1.1.1970
+echo "<br>";
+var_dump($datum2);
